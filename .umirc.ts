c0d698632a12ce67e441f27e06c9ce14c0bc1caa
AllenTom <qweasdzxczse2@gmail.com>
2020-07-14 17:55:59 +0800

    init project

---
+++ .umirc.ts
@@ -0,0 +1,22 @@
+const config =  {
+  routes: [
+    {
+      path: '/',
+      component: '../layouts/index',
+      routes: [
+        { path: '/', component: '../pages/index' }
+      ]
+    }
+  ],
+  locale: {
+    default: 'zh-CN',
+    antd: true,
+    title: false,
+    baseNavigator: true,
+    baseSeparator: '-',
+  },
+  history: { type: 'hash' },
+  publicPath: './',
+}
+
+export default config;
